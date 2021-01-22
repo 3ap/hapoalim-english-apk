243b5aaf50e6fb477521dba4aaf088a2e5452d1c
Sergey Nazaryev <sergey@nazaryev.com>
2021-01-22 20:30:33 +0200

    helpers: Un-hardcode version and remove Russian language

--- helpers/build.sh
+++ helpers/build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 set -efux
 
-apk=../hapoalim-20201106.apk
-tempdir=../hapoalim-20201106.apk-decompiled
+version=30.9.0
+apk="../hapoalim-${version}.apk"
+tempdir="../hapoalim-${version}.apk-decompiled"
 
 rm -rf "${tempdir}"
 java -Xmx256m -jar apktool_2.4.1.jar d -o "${tempdir}" "${apk}"
 
-cp -r ../patch/30.7.1/assets \
-      ../patch/30.7.1/res \
-      ../patch/30.7.1/smali_classes2 \
-      ../patch/30.7.1/AndroidManifest.xml \
+cp -r "../patch/${version}/assets" \
+      "../patch/${version}/res" \
+      "../patch/${version}/smali_classes2" \
+      "../patch/${version}/AndroidManifest.xml" \
       "${tempdir}"
 
 ag "com\.ideomobile\.hapoalim" -l -r "${tempdir}" | tr -d '\r' | sort | uniq | xargs -P 8 -I {} sed -i'' -b 's,com\.ideomobile\.hapoalim,com\.ideomobile\.hapoalimalt,g' {}
 ag "Lcom/ideomobile/hapoalim"  -l -r "${tempdir}" | tr -d '\r' | sort | uniq | xargs -P 8 -I {} sed -i'' -b 's,Lcom/ideomobile/hapoalim,Lcom/ideomobile/hapoalimalt,g' {}
-#mv "${tempdir}/smali_classes2/com/ideomobile/hapoalim" "${tempdir}/smali_classes2/com/ideomobile/hapoalimalt"
 
 java -Xmx256m -jar apktool_2.4.1.jar b "${tempdir}"
 
@@ -29,6 +29,6 @@ keystore=debug.keystore
 
 java -Xmx256m -jar uber-apk-signer-1.1.0.jar \
               --ksDebug "${keystore}" \
-              -a "${tempdir}/dist/hapoalim-20201106.apk" \
+              -a "${tempdir}/dist/hapoalim-${version}.apk" \
               --allowResign \
               --overwrite
--- helpers/update.sh
+++ helpers/update.sh
@@ -5,24 +5,21 @@ curl -L "https://docs.google.com/spreadsheets/d/1sYOnptgxa07OjTEfMGcHCy8G9dLBG7K
 curl -L "https://docs.google.com/spreadsheets/d/1sYOnptgxa07OjTEfMGcHCy8G9dLBG7KiZJPDwC-onSs/export?format=csv&id=1sYOnptgxa07OjTEfMGcHCy8G9dLBG7KiZJPDwC-onSs&gid=445215103" > strings.xml.csv
 curl -L "https://docs.google.com/spreadsheets/d/1sYOnptgxa07OjTEfMGcHCy8G9dLBG7KiZJPDwC-onSs/export?format=csv&id=1sYOnptgxa07OjTEfMGcHCy8G9dLBG7KiZJPDwC-onSs&gid=687797133" > arrays.xml.csv
 
-patchdir=../patch/30.7.1
+version=30.9.0
+patchdir="../patch/30.9.0"
+language=english
 
-./mutual_dictionary.py "english" > "${patchdir}/assets/static/mutual_dictionary_new.json"
+./mutual_dictionary.py "${language}" > "${patchdir}/assets/static/mutual_dictionary_new.json"
 
-mkdir -p "${patchdir}/res/values-en"
-mkdir -p "${patchdir}/res/values-ru"
+mkdir -p "${patchdir}/res/values"
+./arrays.py "${language}" > "${patchdir}/res/values/arrays.xml"
+./strings.py "${language}" > "${patchdir}/res/values/strings.xml"
 
-./arrays.py "english" > "${patchdir}/res/values-en/arrays.xml"
-./arrays.py "russian" > "${patchdir}/res/values-ru/arrays.xml"
+sed -i -e 's/<?xml version="1.0" ?><resources>/<?xml version="1.0" encoding="utf-8"?>\n<resources>/g' "${patchdir}/res/values/strings.xml"
 
-./strings.py "english" > "${patchdir}/res/values-en/strings.xml"
-./strings.py "russian" > "${patchdir}/res/values-ru/strings.xml"
-
-sed -i -e 's/<?xml version="1.0" ?><resources>/<?xml version="1.0" encoding="utf-8"?>\n<resources>/g' "${patchdir}/res/values-en/strings.xml"
-sed -i -e 's/<?xml version="1.0" ?><resources>/<?xml version="1.0" encoding="utf-8"?>\n<resources>/g' "${patchdir}/res/values-ru/strings.xml"
-
-unix2dos "${patchdir}/res/values-en/strings.xml"
-unix2dos "${patchdir}/res/values-ru/strings.xml"
+sed -i -e 's/<?xml version="1.0" ?><resources>/<?xml version="1.0" encoding="utf-8"?>\n<resources>/g' "${patchdir}/res/values/arrays.xml"
+sed -i -e 's,<array name="null_"/>,<array name="null_" />,g' "${patchdir}/res/values/arrays.xml"
+sed -i -e 's/&quot;/"/g' "${patchdir}/res/values/arrays.xml"
 
 ./mutual_dictionary.py csv > new-mutual_dictionary_new.csv
 ./arrays.py csv > new-arrays.xml.csv
